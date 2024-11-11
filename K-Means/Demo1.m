730c1c166c1b1ab3e4d67d0b3cae6eb25e2625fc
ShahinKhalkhali <Shahin.khalkhali94@hotmail.com>
2024-11-11 15:44:02 -0500

    Euclidean distance completed

--- K-Means/Demo1.m
+++ K-Means/Demo1.m
@@ -10,7 +10,9 @@ clc
 % Read and display an image
 load Toy_T1_T2.mat
 % Remove the following 3 variables underneath to revert to original
-
+t1 = [0.4, 0.5; 0.25, 0.2];
+t2 = [0.2, 0.6; 0.25, 0.1];
+pd = [1, 0.35; 0.25, 0.45];
 
 figure, 
 subplot(1,3,1), imagesc(t1)
--- K-Means/KMeans.m
+++ K-Means/KMeans.m
@@ -44,57 +44,100 @@ end
 
 segmentedImage = zeros(M, N); %initialize. This will be the output
 
-% BestDfit = 1e10;  % Just a big number!
+BestDfit = 1e10;  % Just a big number!
 
 % run KMeans NofRadomization times
-for KMeanNo = 1 : NofRadomization
-    new_max_X = 0;
-    new_max_Y = 0;
-    new_max_Z = 0;
-    
+for KMeanNo = 1 : NofRadomization    
 %     % randomize if clusterCentersIn was empty
     if NofRadomization>1
         clusterCentersIn = rand(numberofClusters, noF); %randomize initialization
     end
     
-    % Create 3D array of intensities
+    % Initialize intensity array based on the newly found boundaries
+%     new_max_X = 0;
+%     new_max_Y = 0;
+%     new_max_Z = 0;
+    intensity_t1 = [];
+    intensity_t2 = [];
+    intensity_pd = [];
+    
+    % Find 3D array's intensity boundaries
     for k = 1:noF
         for j = 1:N
             for i = 1:M
                 
-                if k == 1
-                    old_max_X = featureImageIn(i, j, k);  % Initialize peak of X-intensity for counter
-                    % Set new max of X-plane
-                    if new_max_X < old_max_X
-                        new_max_X = old_max_X;
-                    end
+                if k == 1 % Layer 1 (t1)
+%                     old_max_X = featureImageIn(i, j, k);  % Initialize peak of X-intensity for counter
+%                     % Set new max of X-plane
+%                     if new_max_X < old_max_X
+%                         new_max_X = old_max_X;
+%                     end
+                    % Store pixel at current location
+                    intensity_t1 = [intensity_t1, featureImageIn(i,j,k)];
+                end
+
+                if k == 2 % Layer 2 (t2)
+%                     old_max_Y = featureImageIn(i, j, k);  % Initialize peak of Y-intensity for counter
+%                     % Set new max of Y-plane
+%                     if new_max_Y < old_max_Y
+%                         new_max_Y = old_max_Y;
+%                     end
+                    % Store pixel at current location
+                    intensity_t2 = [intensity_t2, featureImageIn(i,j,k)];
                 end
                 
-                if k == 2
-                    old_max_Y = featureImageIn(i, j, k);  % Initialize peak of Y-intensity for counter
-                    % Set new max of Y-plane
-                    if new_max_Y < old_max_Y
-                        new_max_Y = old_max_Y;
-                    end 
-                end
-                
-                if k == 3
-                    old_max_Z = featureImageIn(i, j, k);  % Initialize peak of Z-intensity for counter
-                    % Set new max of Z-plane
-                    if new_max_Z < old_max_Z
-                        new_max_Z = old_max_Z;
-                    end
+                if k == 3 % Layer 3 (pd)
+%                     old_max_Z = featureImageIn(i, j, k);  % Initialize peak of Z-intensity for counter
+%                     % Set new max of Z-plane
+%                     if new_max_Z < old_max_Z
+%                         new_max_Z = old_max_Z;
+%                     end
+                    % Store pixel at current location
+                    intensity_pd = [intensity_pd, featureImageIn(i,j,k)];
                 end
                 
             end
         end
     end
     
-    % Store image intensities in newly made 3D array
+    % Checks maximum dimensions of 3D intensity array
+%     intensities = [new_max_X; new_max_Y; new_max_Z];    % Checks max possible axis of intensity_array
+    
+    % Intensities in newly made 3D array
+    intensity_array = [intensity_t1; intensity_t2; intensity_pd];       % Each column is a pixel in space
+    clusterCentersIn
+    points = intensity_array'
+    [lenght_points_X, length_point_Y] = size(points);
     
     
+    % Compare image's intensity with clusters and store them in updating toy array
+    euclidean = zeros(lenght_points_X, length_point_Y);
+    if lenght_points_X < numberofClusters
     
-    new_max_X
-    new_max_Y
-    new_max_Z
+        for x = 1:lenght_points_X % Go along columns
+        
+            for y = 1:length_point_Y % Go along rows
+
+                current_point = points(x,y)
+                current_cluster = clusterCentersIn(x,y)
+                euclidean(x,y) = abs((current_point - current_cluster)^2)
+
+            end
+            euclidean(x,:)
+            distance_toy(x) = sqrt(sum(euclidean(x,:)))
+            
+        end
+    
+    % Compare image's intensity with clusters and store them in updating actual array
+    else
+        
+        for x = 1:numberofClusters % Go along columns
+            for y = 1:length_point_Y % Go along rows
+                distance(x,y) = sqrt((clusterCentersIn(x,y))^2 + (points(x,y))^2)
+            end
+        end
+        
+    end
+    
+end
 end
